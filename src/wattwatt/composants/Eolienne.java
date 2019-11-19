1f86c84c94b621083f8a7e07c6065698c620c1ef
Asparodia <thiernobah91860@yahoo.fr>
2019-11-19 18:26:40 +0100

    réorganisation

---
+++ src/wattwatt/composants/Eolienne.java
@@ -0,0 +1,322 @@
+package wattwatt.composants;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
+import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.exceptions.ComponentShutdownException;
+import fr.sorbonne_u.components.exceptions.ComponentStartException;
+import fr.sorbonne_u.components.interfaces.DataOfferedI;
+import wattwatt.data.StringData;
+import wattwatt.interfaces.IStringDataOffered;
+import wattwatt.interfaces.IStringDataRequired;
+import wattwatt.ports.StringDataInPort;
+import wattwatt.ports.StringDataOutPort;
+
+/**
+ * La classe <code>Eolienne</code> qui represente le composant Eolienne.
+ * L'eolienne va produire de l'electricite en fonction du vent et peut
+ * necessitee d'etre rabattu en fonction des conditions meteorologiques.
+ * 
+ * <p>
+ * Created on : 2019-11-09
+ * </p>
+ * 
+ * @author Thierno BAH, Pascal ZHENG
+ *
+ */
+
+public class Eolienne extends AbstractComponent implements IStringDataOffered, IStringDataRequired {
+
+	/**
+	 * Les ports par lesquels l'eolienne envoie des donnees representees par la
+	 * classe StringData
+	 */
+	public HashMap<String, StringDataInPort> stringDataInPort;
+
+	/**
+	 * Les ports par lesquels l'eolienne recoit des donnees representees par la
+	 * classe StringData
+	 */
+	public HashMap<String, StringDataOutPort> stringDataOutPort;
+
+	/**
+	 * La liste des messages recues, representees par la classe StringData.
+	 */
+	Vector<StringData> messages_recus = new Vector<>();
+
+	/**
+	 * La liste des messages a envoyer
+	 */
+	protected ConcurrentHashMap<String, Vector<StringData>> messages_envoyes = new ConcurrentHashMap<>();
+
+	/**
+	 * URI du composant
+	 */
+	public String URI;
+
+	/**
+	 * Liste des uris
+	 */
+	protected Vector<String> uris;
+
+	/**
+	 * Objet permettant de declencher un comportement
+	 */
+	protected Timer timer = new Timer();
+
+	/**
+	 * Pour l'instant la valeur produite par l'eolienne est decidee aléatoirement
+	 */
+	protected Random rand = new Random();
+
+	/**
+	 * Definit si l'eolienne est en marche ou non
+	 */
+	protected boolean isOn = false;
+
+	/**
+	 * Quantite d'electricite produite
+	 */
+	protected int val = 0;
+
+	/**
+	 * create a passive component if both <code>nbThreads</code> and
+	 * <code>nbSchedulableThreads</code> are both zero, and an active one with
+	 * <code>nbThreads</code> non schedulable thread and
+	 * <code>nbSchedulableThreads</code> schedulable threads otherwise.
+	 * 
+	 * <p>
+	 * <strong>Contract</strong>
+	 * </p>
+	 * 
+	 * <pre>
+	 * pre	reflectionInboundPortURI != null
+	 * pre	nbThreads &gt;= 0
+	 * pre	nbSchedulableThreads &gt;= 0
+	 * post	true			// no postcondition.
+	 * </pre>
+	 * 
+	 * @param reflectionInboundPortURI URI of the inbound port offering the
+	 *                                 <code>ReflectionI</code> interface.
+	 * @param nbThreads                number of threads to be created in the
+	 *                                 component pool.
+	 * @param nbSchedulableThreads     number of threads to be created in the
+	 *                                 component schedulable pool.
+	 * @throws Exception
+	 */
+	public Eolienne(String reflectionInboundPortURI, int nbThreads, int nbSchedulableThreads) throws Exception {
+		super(reflectionInboundPortURI, nbThreads, nbSchedulableThreads);
+		URI = reflectionInboundPortURI;
+		this.stringDataInPort = new HashMap<>();
+		this.stringDataOutPort = new HashMap<>();
+	}
+
+	public Eolienne(String uri, int nbThreads, int nbSchedulableThreads, Vector<String> uris) throws Exception {
+		super(uri, nbThreads, nbSchedulableThreads);
+		this.URI = uri;
+		this.addOfferedInterface(IStringDataOffered.class);
+		this.addOfferedInterface(DataOfferedI.PullI.class);
+		this.stringDataInPort = new HashMap<>();
+		this.stringDataOutPort = new HashMap<>();
+		this.uris = uris;
+		updateURI();
+	}
+
+	@Override
+	public void start() throws ComponentStartException {
+		super.start();
+		this.logMessage("Eolienne starting");
+		this.runTask(new AbstractTask() {
+			public void run() {
+				try {
+					Thread.sleep(100);
+					String msg = "hello je suis eolienne";
+					envoieString("controleur", msg);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	@Override
+	public void execute() throws Exception {
+		super.execute();
+	}
+
+	@Override
+	public void shutdown() throws ComponentShutdownException {
+		this.logMessage("Eolienne shutdown");
+		timer.cancel();
+		try {
+			for (String s : stringDataOutPort.keySet()) {
+				stringDataOutPort.get(s).unpublishPort();
+			}
+			for (String s : stringDataInPort.keySet()) {
+				stringDataInPort.get(s).unpublishPort();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		super.shutdown();
+	}
+
+	@Override
+	public void finalise() throws Exception {
+		try {
+			for (String s : stringDataOutPort.keySet()) {
+				stringDataOutPort.get(s).unpublishPort();
+			}
+			for (String s : stringDataInPort.keySet()) {
+				stringDataInPort.get(s).unpublishPort();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		super.finalise();
+	}
+
+	/**
+	 * Creer une connexion entre <code> uriCible </code> et l'appareil
+	 * 
+	 * <p>
+	 * <strong>Contract</strong>
+	 * </p>
+	 * 
+	 * <pre>
+	 * pre	uriCible != null
+	 * pre	in != null
+	 * pre	out != null
+	 * post	true			// no postcondition.
+	 * </pre>
+	 * 
+	 * @param uriCible uri du composant a connecter
+	 * @param in       nom du DataInPort de uriCible
+	 * @param out      nom du DataOutPort de uriCible
+	 * @throws Exception
+	 */
+	public void plug(String uriCible, String in, String out) throws Exception {
+		this.stringDataInPort.put(uriCible, new StringDataInPort(in, this));
+		this.addPort(stringDataInPort.get(uriCible));
+		this.stringDataInPort.get(uriCible).publishPort();
+		this.stringDataOutPort.put(uriCible, new StringDataOutPort(out, this));
+		this.addPort(stringDataOutPort.get(uriCible));
+		this.stringDataOutPort.get(uriCible).publishPort();
+	}
+
+	@Override
+	public void getMessage(StringData msg) throws Exception {
+		messages_recus.add(msg);
+		this.logMessage("Eolienne recoit : " + messages_recus.remove(0).getMessage());
+		switch (msg.getMessage()) {
+		case "switchOn":
+			if (!isOn) {
+				isOn = true;
+				this.logMessage("Demarrage de l'eolienne");
+			}
+			timer.schedule(new ProductionTask(this), 0, 3000);
+			break;
+		case "switchOff":
+			if (isOn) {
+				isOn = false;
+				this.logMessage("Arret de l'eolienne");
+			}
+			timer.purge();
+			break;
+		case "shutdown":
+			shutdown();
+			break;
+		}
+	}
+
+	/**
+	 * Envoie le message <code>msg</code> sur le composant d'URI <code>uri</code>
+	 * 
+	 * @param uri URI du composant vers lequel on veut envoyer <code>msg</code>
+	 * @param msg message à envoyer
+	 * @throws Exception
+	 */
+	public void envoieString(String uri, String msg) throws Exception {
+		StringData m = new StringData();
+		m.setMessage(msg);
+		messages_envoyes.put(uri, new Vector<StringData>());
+		messages_envoyes.get(uri).add(m);
+		sendMessage(uri);
+	}
+
+	@Override
+	public StringData sendMessage(String uri) throws Exception {
+		StringData m = messages_envoyes.get(uri).get(0);
+		messages_envoyes.get(uri).remove(m);
+		this.stringDataInPort.get(uri).send(m);
+		return m;
+	}
+
+	/**
+	 * Affiche la production de l'eolienne
+	 * @throws Exception 
+	 */
+	public void print() throws Exception {
+		// Il faut faire un truc de prod plus realiste
+		this.logMessage("Eolienne tourne: production de " + this.val + " kW");
+		envoieString("controleur", "eolienne:" + this.val);
+	}
+
+	/**
+	 * Methode permettant d'attribuer des DataIn et DataOut aux differentes URI
+	 * 
+	 * @throws Exception
+	 */
+	public void updateURI() throws Exception {
+		for (String appareilURI : uris) {
+			String randomURIPort = java.util.UUID.randomUUID().toString();
+			this.stringDataInPort.put(appareilURI, new StringDataInPort(randomURIPort, this));
+			this.addPort(stringDataInPort.get(appareilURI));
+			this.stringDataInPort.get(appareilURI).publishPort();
+
+			randomURIPort = java.util.UUID.randomUUID().toString();
+			this.stringDataOutPort.put(appareilURI, new StringDataOutPort(randomURIPort, this));
+			this.addPort(stringDataOutPort.get(appareilURI));
+			this.stringDataOutPort.get(appareilURI).publishPort();
+		}
+	}
+
+	/**
+	 * Classe permettant d'afficher a intervalles reguliers la production
+	 * electrique. Elle herite donc de TimerTask pour simuler ces intervalles.
+	 * 
+	 * <p>
+	 * Created on : 2019-11-16
+	 * </p>
+	 * 
+	 * @author Thierno BAH, Pascal ZHENG
+	 *
+	 */
+	class ProductionTask extends TimerTask {
+		Eolienne e;
+
+		public ProductionTask(Eolienne e) {
+			this.e = e;
+		}
+
+		@Override
+		public void run() {
+			if (e.isOn) {
+				this.e.val = 1200 + rand.nextInt(500);
+				try {
+					e.print();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
---
+++ src/wattwatt/tests/DistributedCVM.java
@@ -0,0 +1,198 @@
+package wattwatt.tests;
+
+import java.util.Vector;
+
+import fr.sorbonne_u.components.cvm.AbstractDistributedCVM;
+import wattwatt.composants.Batterie;
+import wattwatt.composants.Compteur;
+import wattwatt.composants.Controleur;
+import wattwatt.composants.Eolienne;
+import wattwatt.composants.LaveLinge;
+import wattwatt.composants.Refrigerateur;
+import wattwatt.composants.SecheCheveux;
+import wattwatt.connecteurs.StringDataConnector;
+
+public class DistributedCVM extends AbstractDistributedCVM {
+
+	/**
+	 * URI of the reflection inbound port of the concurrent map component.
+	 */
+	protected String COMPTEUR_URI = "compteur";
+	protected String CONTROLLEUR_URI = "controleur";
+	protected String LAVE_LINGE_URI = "laveLinge";
+	protected String EOLIENNE_URI = "eolienne";
+	protected String BATTERIE_URI = "batterie";
+	protected String REFRIGERATEUR_URI = "refrigerateur";
+
+	String outportCont = "outPortCont";
+	String inportCont = "inPortCont";
+
+	String outportCont2 = "outPortCont2";
+	String inportCont2 = "inPortCont2";
+
+	String outportCont3 = "outPortCont3";
+	String inportCont3 = "inPortCont3";
+
+	String outportCont4 = "outPortCont4";
+	String inportCont4 = "inPortCont4";
+
+	String outportCont5 = "outPortCont5";
+	String inportCont5 = "inPortCont5";
+
+	String outportCpt = "outPortCpt";
+	String inportCpt = "inPortCpt";
+
+	String outportEol = "outPortEol";
+	String inportEol = "inPortEol";
+
+	String outportBat = "outPortBat";
+	String inportBat = "inPortBat";
+
+	String outportLav = "outPortLav";
+	String inportLav = "inPortLav";
+
+	String outportRef = "outPortRef";
+	String inportRef = "inPortRef";
+
+	protected Vector<String> uris = new Vector<>();
+
+	Controleur cont;
+	Compteur cpt;
+	SecheCheveux secheCheveux;
+	LaveLinge laveLinge;
+	Eolienne eolienne;
+	Batterie batterie;
+	Refrigerateur refrigerateur;
+
+	int quantiteMaxBatterie = 10;
+
+	public DistributedCVM(String[] args, int xLayout, int yLayout) throws Exception {
+		super(args, xLayout, yLayout);
+		uris.add(COMPTEUR_URI);
+		uris.add(EOLIENNE_URI);
+		uris.add(BATTERIE_URI);
+		uris.add(LAVE_LINGE_URI);
+		uris.add(REFRIGERATEUR_URI);
+	}
+
+	@Override
+	public void instantiateAndPublish() throws Exception {
+
+		if (thisJVMURI.equals(CONTROLLEUR_URI)) {
+			this.cont = new Controleur(CONTROLLEUR_URI, 1, 0);
+			this.cont.plug(COMPTEUR_URI, inportCont, outportCont);
+			this.cont.plug(EOLIENNE_URI, inportCont2, outportCont2);
+			this.cont.plug(BATTERIE_URI, inportCont3, outportCont3);
+			this.cont.plug(LAVE_LINGE_URI, inportCont4, outportCont4);
+			this.cont.plug(REFRIGERATEUR_URI, inportCont5, outportCont5);
+
+			this.addDeployedComponent(CONTROLLEUR_URI, cont);
+			this.toggleTracing(CONTROLLEUR_URI);
+
+		} else if (thisJVMURI.equals(COMPTEUR_URI)) {
+			this.cpt = new Compteur(COMPTEUR_URI, 1, 0);
+			this.cpt.plug(CONTROLLEUR_URI, inportCpt, outportCpt);
+			this.addDeployedComponent(COMPTEUR_URI, cpt);
+			this.toggleTracing(COMPTEUR_URI);
+
+		} else if (thisJVMURI.equals(EOLIENNE_URI)) {
+			this.eolienne = new Eolienne(EOLIENNE_URI, 1, 0);
+			this.eolienne.plug(CONTROLLEUR_URI, inportEol, outportEol);
+			this.addDeployedComponent(EOLIENNE_URI, eolienne);
+			this.toggleTracing(EOLIENNE_URI);
+
+		} else if (thisJVMURI.equals(BATTERIE_URI)) {
+			this.batterie = new Batterie(BATTERIE_URI, 1, 0, quantiteMaxBatterie);
+			this.batterie.plug(CONTROLLEUR_URI, inportBat, outportBat);
+			this.addDeployedComponent(BATTERIE_URI, batterie);
+			this.toggleTracing(BATTERIE_URI);
+
+		} else if (thisJVMURI.equals(LAVE_LINGE_URI)) {
+			this.laveLinge = new LaveLinge(LAVE_LINGE_URI, 1, 0);
+			this.laveLinge.plug(CONTROLLEUR_URI, inportLav, outportLav);
+			this.addDeployedComponent(LAVE_LINGE_URI, laveLinge);
+			this.toggleTracing(LAVE_LINGE_URI);
+
+		} else if (thisJVMURI.equals(REFRIGERATEUR_URI)) {
+			this.refrigerateur = new Refrigerateur(REFRIGERATEUR_URI, 1, 0);
+			this.refrigerateur.plug(CONTROLLEUR_URI, inportRef, outportRef);
+			this.addDeployedComponent(REFRIGERATEUR_URI, refrigerateur);
+			this.toggleTracing(REFRIGERATEUR_URI);
+
+		} else {
+			throw new RuntimeException("Unknown JVM URI: " + thisJVMURI);
+		}
+		super.instantiateAndPublish();
+	}
+
+	/**
+	 * interconnect the components.
+	 * 
+	 * <p>
+	 * <strong>Contract</strong>
+	 * </p>
+	 * 
+	 * <pre>
+	 * pre	true				// no more preconditions.
+	 * post	true				// no more postconditions.
+	 * </pre>
+	 * 
+	 * @see fr.sorbonne_u.components.cvm.AbstractDistributedCVM#interconnect()
+	 */
+	@Override
+	public void interconnect() throws Exception {
+		assert this.isIntantiatedAndPublished();
+
+		if (thisJVMURI.equals(CONTROLLEUR_URI)) {
+			this.doPortConnection(CONTROLLEUR_URI, this.cont.stringDataInPort.get(COMPTEUR_URI).getPortURI(),
+					this.outportCpt, StringDataConnector.class.getCanonicalName());
+			this.doPortConnection(CONTROLLEUR_URI, this.cont.stringDataInPort.get(EOLIENNE_URI).getPortURI(),
+					this.outportEol, StringDataConnector.class.getCanonicalName());
+			this.doPortConnection(CONTROLLEUR_URI, this.cont.stringDataInPort.get(BATTERIE_URI).getPortURI(),
+					this.outportBat, StringDataConnector.class.getCanonicalName());
+			this.doPortConnection(CONTROLLEUR_URI, this.cont.stringDataInPort.get(LAVE_LINGE_URI).getPortURI(),
+					this.outportLav, StringDataConnector.class.getCanonicalName());
+			this.doPortConnection(CONTROLLEUR_URI, this.cont.stringDataInPort.get(REFRIGERATEUR_URI).getPortURI(),
+					this.outportRef, StringDataConnector.class.getCanonicalName());
+
+		} else if (thisJVMURI.equals(COMPTEUR_URI)) {
+			this.doPortConnection(COMPTEUR_URI, this.cpt.stringDataInPort.get(CONTROLLEUR_URI).getPortURI(),
+					this.outportCont, StringDataConnector.class.getCanonicalName());
+
+		} else if (thisJVMURI.equals(EOLIENNE_URI)) {
+			this.doPortConnection(EOLIENNE_URI, this.eolienne.stringDataInPort.get(CONTROLLEUR_URI).getPortURI(),
+					this.outportCont2, StringDataConnector.class.getCanonicalName());
+
+		} else if (thisJVMURI.equals(BATTERIE_URI)) {
+			this.doPortConnection(BATTERIE_URI, this.batterie.stringDataInPort.get(CONTROLLEUR_URI).getPortURI(),
+					this.outportCont3, StringDataConnector.class.getCanonicalName());
+
+		} else if (thisJVMURI.equals(LAVE_LINGE_URI)) {
+			this.doPortConnection(LAVE_LINGE_URI, this.laveLinge.stringDataInPort.get(CONTROLLEUR_URI).getPortURI(),
+					this.outportCont4, StringDataConnector.class.getCanonicalName());
+
+		} else if (thisJVMURI.equals(REFRIGERATEUR_URI)) {
+			this.doPortConnection(REFRIGERATEUR_URI,
+					this.refrigerateur.stringDataInPort.get(CONTROLLEUR_URI).getPortURI(), this.outportCont5,
+					StringDataConnector.class.getCanonicalName());
+		} else {
+
+			System.out.println("Unknown JVM URI... " + thisJVMURI);
+
+		}
+
+		super.interconnect();
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			DistributedCVM da = new DistributedCVM(args, 2, 5);
+			da.startStandardLifeCycle(500000L);
+			Thread.sleep(500000L);
+			System.exit(0);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
