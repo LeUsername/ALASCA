c2bed4180651f324b7cade2c9367d76f7e77c703
Asparodia <thiernobah91860@yahoo.fr>
2019-12-06 17:01:09 +0100

    interface groupe electrogene + tools

--- src/wattwattReborn/interfaces/sources/intermittent/IGroupeElectrogene.java
+++ src/wattwattReborn/interfaces/sources/intermittent/IGroupeElectrogene.java
@@ -1,5 +1,14 @@
 package wattwattReborn.interfaces.sources.intermittent;
 
-public interface IGroupeElectrogene {
+import wattwattReborn.interfaces.sources.ISources;
+
+public interface IGroupeElectrogene extends ISources {
+	
+	public boolean fuelIsEmpty() throws Exception;
+	public boolean fuelIsFull() throws Exception;
+	public int fuelQuantity() throws Exception;
+	
+	public void start() throws Exception; // use fuel to prod electricity
+	public void stop() throws Exception;
 
 }
---
+++ src/wattwattReborn/tools/GroupeElectrogene/GroupreElectrogeneReglage.java
@@ -0,0 +1,9 @@
+package wattwattReborn.tools.GroupeElectrogene;
+
+public class GroupreElectrogeneReglage {
+
+	public static final int FUEL_CAPACITY = 1000;
+	public static final int PROD_THR = 100;
+	
+	public static final int REGUL_RATE = 1000;
+}
